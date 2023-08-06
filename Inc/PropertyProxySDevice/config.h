f327c27aaf74155a11dfe984bc6a5605f43ed1b5
Ivan Shestakov <vanyww@mail.ru>
2023-08-06 11:49:05 +0300

    Add PROPERTY_PROXY_SDEVICE_USE_ROLLBACK doxygen-definition to config.h

--- Inc/PropertyProxySDevice/config.h
+++ Inc/PropertyProxySDevice/config.h
@@ -5,3 +5,8 @@
 #if defined(PROPERTY_PROXY_SDEVICE_USE_EXTERNAL_CONFIG)
 #include "property_proxy_sdevice_config.h"
 #endif
+
+#if defined(DOXYGEN)
+#define PROPERTY_PROXY_SDEVICE_USE_ROLLBACK
+#undef PROPERTY_PROXY_SDEVICE_USE_ROLLBACK
+#endif
